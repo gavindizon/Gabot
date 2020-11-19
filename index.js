a2a3a3cea6a94577d523d302cf6da87f7dac04dd
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2020-11-19 17:46:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const fs = require("fs");
-const Discord = require("discord.js");
+
+const { Client, Intents, Collection } = require("discord.js");
+const intents = new Intents([
+  Intents.NON_PRIVILEGED, // include all non-privileged intents, would be better to specify which ones you actually need
+  "GUILD_MEMBERS", // lets you request guild members (i.e. fixes the issue)
+  "GUILD_PRESENCES",
+]);
 
 require("dotenv").config();
 
-const gabot = new Discord.Client();
-gabot.commands = new Discord.Collection();
+const gabot = new Client({ ws: { intents } });
+gabot.commands = new Collection();
+var server;
+var tc;
 
 const commandFiles = fs
   .readdirSync("./commands")
@@ -14,21 +22,44 @@ for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   gabot.commands.set(command.name, command);
 }
-var server;
+
 gabot.on("ready", (m) => {
   server = gabot.guilds.cache.get("274436634823491584");
   gabot.user.setActivity("Genshin Impact", { type: "PLAYING" });
+
+  server.members.fetch().then(console.log).catch(console.error);
+  tc = server.channels.cache.get("778913513371992064");
+  tc.send("I am Online!");
+
+  //server.channels.create("Bot-created Channel").catch(console.error);
 });
 
 gabot.on("message", async (message) => {
   if (message.content.startsWith(process.env.PREFIX + "rolelist")) {
-    const Members = message.guild.members.cache.map((member) => {
+    const Members = server.members.cache.map((member) => {
       return member.user.username;
     });
 
     message.channel.send(`Users: ${Members}`);
   }
 });
+gabot.on("presenceUpdate", (oldPresence, newPresence) => {
+  if (newPresence.status === "offline" || newPresence.activities.length === 0)
+    return;
+
+  tc.send(
+    `${newPresence.user.username} is now **${newPresence.activities[0].type}** ${newPresence.activities[0].name}`
+  );
+
+  tc.messages.fetch({ limit: 1 }).then((messages) => {
+    tc.bulkDelete(messages);
+  });
+});
+
+gabot.on("guildMemberUpdate", (oldMember, newMember) => {
+  tc.send("Someone Updated shit");
+});
+
 gabot.on("message", (message) => {
   if (!message.content.startsWith(process.env.PREFIX) || message.author.bot)
     return;
